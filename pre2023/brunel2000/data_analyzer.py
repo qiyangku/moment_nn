fixed oscillation frequency calculation; added quartiles

pre2023/brunel2000/data_analyzer.py
@@ -37,8 +37,12 @@ mean_pop_avg = np.zeros(size)
 ff_pop_avg = np.zeros(size)
 mean_pop_std = np.zeros(size)
 ff_pop_std = np.zeros(size)
+mean_quartiles = np.zeros(size+(2,))
+ff_quartiles = np.zeros(size+(2,))
+
 
 osc_amp = np.zeros(size)
+osc_amp_ff = np.zeros(size)
 osc_freq = np.zeros(size)
 
 for i in range(size[0]):
@@ -52,27 +56,32 @@ for i in range(size[0]):
         config = dat['config'].item()
         
         # average over second half of simulation, to deal with oscillating solutions
-        cut_off = int(u.shape[1]/2)
+        cut_off = int(u.shape[1]/2) #discard the first half of time series
         u_time_avg = np.mean(u[:, cut_off:], axis = 1)  #average over time
         ff_time_avg = np.mean(ff[:, cut_off:], axis = 1) 
                
         # population stats
         mean_pop_avg[i,j] = np.mean(u_time_avg)
         ff_pop_avg[i,j] = np.mean(ff_time_avg)
+        
         mean_pop_std[i,j] = np.std(u_time_avg)
         ff_pop_std[i,j] = np.std(ff_time_avg)
         
+        mean_quartiles[i,j,:] = np.percentile(u_time_avg, [25,75])        
+        ff_quartiles[i,j,:] = np.percentile(ff_time_avg, [25,75])
+        
+        
         # detect oscillation
         tmp = np.mean(u[:, cut_off:], axis=0) #population average, no time average
-                
+        tmp_ff = np.mean(ff[:, cut_off:], axis=0)
         if ie_ratio[j]>4: # no oscilation found for excitation dominant regime
             osc_amp[i,j] = 0.5*(np.max(tmp)-np.min(tmp)) #rough estimate of oscillation amplitude
-            
+            osc_amp_ff[i,j] = 0.5*(np.max(tmp_ff)-np.min(tmp_ff))
             #if osc_amp[i,j]>1e-5:
             psd = np.abs(np.fft.fft(tmp))
             psd[0]=0
             psd = psd[:int(len(psd)/2)] #discard mirrored result
-            osc_freq[i,j] = np.argmax(psd)/(config['T_mnn']*0.02)  # psd peak index * df, which is 1/simulation time (ms)
+            osc_freq[i,j] = np.argmax(psd)/(config['T_mnn']/2*0.02)  # psd peak index * df, which is 1/simulation time (0.02 s is mem constant)
             
 
 dat = {'ie_ratio':ie_ratio,
@@ -83,6 +92,9 @@ dat = {'ie_ratio':ie_ratio,
 'ff_pop_std':ff_pop_std,
 'osc_amp':osc_amp,
 'osc_freq':osc_freq,
+'osc_amp_ff':osc_amp_ff,
+'mean_quartiles':mean_quartiles,
+'ff_quartiles':ff_quartiles,
 }
 
 np.savez(path+'post_analysis.npz', **dat)

pre2023/plot_prod_fig_brunel2000.py
@@ -376,9 +376,9 @@ ie_ratio_array = meta_dat['ie_ratio_array']
 
 plt.close('all')
 plot_example(path, 15)
-plot_example(path, 17) #crit point 1
+#plot_example(path, 17) #crit point 1; very slow convergence as expected
 plot_example(path, 25)
-plot_example(path, 32) #crit point 2
+#plot_example(path, 32) #crit point 2; oscillation slowly decay away
 plot_example(path, 35)
 # i = np.where(uext_array==20)[0][0]
 # plt.close('all')